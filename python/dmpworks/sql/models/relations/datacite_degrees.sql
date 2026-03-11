9e93dc701373910fea470756179ecf0a3b7c2d29
Jamie Diprose <5715104+jdddog@users.noreply.github.com>
2026-03-12 12:16:20 +1300

    Update

--- python/dmpworks/sql/models/relations/datacite_degrees.sql
+++ python/dmpworks/sql/models/relations/datacite_degrees.sql
@@ -14,7 +14,7 @@ MODEL (
 PRAGMA threads=CAST(@VAR('relations_datacite_degrees_threads') AS INT64);
 
 WITH unnested_relations AS (
-  SELECT
+  SELECT DISTINCT
     dc.doi AS work_doi,
     r.related_identifier AS related_doi,
     r.relation_type
@@ -27,6 +27,14 @@ SELECT
   work_doi,
   related_doi,
   relation_type,
+
+  -- For one specific work_doi, how many different related_dois with a specific relation_type does it point out to?
   COUNT(*) OVER (PARTITION BY work_doi, relation_type) AS out_degree,
+
+  -- For one specific related_doi, how many different work_dois with a specific relation_type are pointing in at it?
   COUNT(*) OVER (PARTITION BY related_doi, relation_type) AS in_degree
 FROM unnested_relations;
+
+
+
+
